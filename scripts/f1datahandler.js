2792d12b5eb73df57d8d1f13b6cece9c69de0a04
aaloksg <56730032+aaloksg@users.noreply.github.com>
2020-10-08 16:35:13 +0200

    Added methods to dataHandler
    Added methods to dataHandler
    getTeamsInSeason
    getTeamsByID

--- scripts/f1datahandler.js
+++ scripts/f1datahandler.js
@@ -1,6 +1,9 @@
 var F1DataVis = F1DataVis || {};
 F1DataVis.dataHandler = F1DataVis.dataHandler || {};
 F1DataVis.data = F1DataVis.data || {};
+F1DataVis.data.racesByYear = {};
+F1DataVis.data.teamIdsByYear = {};
+F1DataVis.data.teamsById = {};
 F1DataVis.data.constructorStandingsByRaceId = {};
 
 F1DataVis.dataHandler.initializeData = function () {
@@ -27,23 +30,23 @@ F1DataVis.dataHandler.getRaceIdsInYear = function ( year ) {
     return raceIds;
 };
 
-F1DataVis.dataHandler.createCnstrctrStndgsByRaceIds = function (year) {
-    var raceIds = F1DataVis.dataHandler.getRaceIdsInYear(year);
+F1DataVis.dataHandler.createCnstrctrStndgsByRaceIds = function ( year ) {
+    var raceIds = F1DataVis.dataHandler.getRaceIdsInYear( year );
     var count = 0, length, i, j, raceId, jump = 5, lowerBound, upperBound;
     length = F1DataVis.data.constructorStandings.length;
 
-    if (F1DataVis.data.constructorStandings) {
-        for (i = 0; i < length; i = i + jump) {
+    if ( F1DataVis.data.constructorStandings ) {
+        for ( i = 0; i < length; i = i + jump ) {
             item = F1DataVis.data.constructorStandings[i];
             raceId = item.raceId;
-            if (raceIds.indexOf(raceId) > -1) {
-                raceIds.splice(raceIds.indexOf(raceId), 1);
-                if (F1DataVis.data.constructorStandingsByRaceId[raceId] === undefined) {
-                    if (i !== 0) {
+            if ( raceIds.indexOf( raceId ) > -1 ) {
+                raceIds.splice( raceIds.indexOf( raceId ), 1 );
+                if ( F1DataVis.data.constructorStandingsByRaceId[raceId] === undefined ) {
+                    if ( i !== 0 ) {
                         lowerBound = i - jump;
-                        for (j = i - 1; j > lowerBound; j--) {
+                        for ( j = i - 1; j > lowerBound; j-- ) {
                             item = F1DataVis.data.constructorStandings[j];
-                            if (item.raceId !== raceId) {
+                            if ( item.raceId !== raceId ) {
                                 break;
                             }
                         }
@@ -54,26 +57,65 @@ F1DataVis.dataHandler.createCnstrctrStndgsByRaceIds = function (year) {
                         lowerBound = i;
                     }
 
-                    for (j = i + 1; j < length; j++) {
+                    for ( j = i + 1; j < length; j++ ) {
                         item = F1DataVis.data.constructorStandings[j];
-                        if (item.raceId !== raceId) {
+                        if ( item.raceId !== raceId ) {
                             break;
                         }
                     }
                     upperBound = j;
-                    F1DataVis.data.constructorStandingsByRaceId[raceId] = F1DataVis.data.constructorStandings.slice(lowerBound, upperBound);
+                    F1DataVis.data.constructorStandingsByRaceId[raceId] = F1DataVis.data.constructorStandings.slice( lowerBound, upperBound );
                     i = upperBound - jump;
                 }
-                if (raceIds.length===0) {
+                if ( raceIds.length === 0 ) {
                     break;
                 }
-                    
+
             }
 
-            
+
         }
-        
-    } 
-     
+
+    }
+
 };
 
+F1DataVis.dataHandler.getTeamsInSeason = function ( year ) {
+    var teams = [], races = F1DataVis.data.racesByYear[year],
+        racesLength = races.length, raceIndex,
+        noOfStandings, standingIndex,
+        constructorStandings, constructorId;
+    if ( F1DataVis.data.teamIdsByYear[year] ) {
+        teams = F1DataVis.data.teamIdsByYear[year];
+    } else {
+        for ( raceIndex = 0; raceIndex < racesLength; raceIndex++ ) {
+            constructorStandings = F1DataVis.data.constructorStandingsByRaceId[races[raceIndex].raceId];
+            noOfStandings = constructorStandings.length;
+            for ( standingIndex = 0; standingIndex < noOfStandings; standingIndex++ ) {
+                constructorId = constructorStandings[standingIndex].constructorId;
+                if ( teams.indexOf( constructorId ) === -1 ) {
+                    teams.push( constructorId );
+                }
+            }
+        }
+        F1DataVis.data.teamIdsByYear[year] = teams;
+    }
+    return teams;
+};
+
+F1DataVis.dataHandler.getTeamsByID = function ( teamId ) {
+    var team, index, length, teams;
+    if ( F1DataVis.data.teamsById[teamId] ) {
+        team = F1DataVis.data.teamsById[teamId];
+    } else {
+        teams = F1DataVis.data.constructors;
+        length = teams.length;
+        for ( index = 0; index < length; index++ ) {
+            if ( teams[index].constructorId === teamId ) {
+                team = teams[index];
+            }
+        }
+        F1DataVis.data.teamsById[teamId] = team;
+    }
+    return team;
+};
